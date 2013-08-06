081f3c0a78359d2018cc68e85d7f83fc3eb36ce8
awinky1 <winks18@gmail.com>
2013-08-06 04:03:51 -0400

    file.php

--- file.php
+++ file.php
@@ -2,10 +2,10 @@
 
   $obj = new account(1000);
   
-  $obj->debit(100, 'Walmart');
-  $obj->credit(200, 'Cash Deposit');
-  $obj->debit(500, 'Target');
-  $obj->credit(250, 'Sears');  
+  $obj->debit(100, 'Walmart', 'Jul 20');
+  $obj->credit(200, 'Cash Deposit', 'Jul 25');
+  $obj->debit(500, 'Target', 'Aug 1');
+  $obj->credit(250, 'Sears', 'Aug 10');  
     $obj->process();
 //    print_r($obj);
 
@@ -19,29 +19,31 @@
       $this->starting_balance = $amount;
       $this->current_balance = $amount;   
     }
-    public function debit($amount, $source) {
+    public function debit($amount, $source, $date) {
       $transaction = array();
       $transaction['type'] = 'debit';
       $transaction['amount'] = $amount;
-      $transaction['source'] = $source; 
+      $transaction['source'] = $source;
+      $transaction['date'] = $date;   
 
       $this->transactions[] = $transaction;
     }
  
-    public function credit($amount, $source) {
+    public function credit($amount, $source, $date) {
       $transaction = array();
       $transaction['type'] = 'credit';
       $transaction['amount'] = $amount;
       $transaction['source'] = $source;
- 
+      $transaction['date'] = $date; 
+
       $this->transactions[] = $transaction;
     }  
    
     public function process() {
-      echo 'Type  |  Source  |   Amount <br>';
+      echo 'Type  |  Source  |   Amount  |  Date  <br>';
         foreach($this->transactions as $transaction) {
        
-      echo $transaction['type'] . ' | ' . $transaction['source'] . ' | ' . $transaction['amount'] . '<br>';
+      echo $transaction['type'] . ' | ' . $transaction['source'] . ' | ' . $transaction['amount'] . ' | ' . $transaction['date'] . '<br>';
  
           if($transaction['type'] == 'debit') {
             $this->current_balance = $this->current_balance - $transaction['amount'];
